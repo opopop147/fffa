d8e47d59f4fe2aa265dabd0f29bf6ea5dcd83c4b
opopop147 <113234617+opopop147@users.noreply.github.com>
2023-03-15 17:50:01 +0000

    add test case (2023-03-15T17:50:01)

--- src/go/main2.go
+++ src/go/main2.go
@@ -1,2 +1,3 @@
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-02-01T14:22:25") }
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-02-01T09:57:32") }
+package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-03-15T17:50:01") }
