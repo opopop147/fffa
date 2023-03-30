2bf4738111e8a0b34da2a9348edb055293e6d8bf
opopop147 <113234617+opopop147@users.noreply.github.com>
2023-03-30 16:23:30 +0000

    add test case (2023-03-30T16:23:30)

--- src/java/App2.java
+++ src/java/App2.java
@@ -3,3 +3,4 @@ public class App26 { public static void main(String[] args) { System.out.println
 public class App63 { public static void main(String[] args) { System.out.println("update 2023-03-14T06:12:01"); } }
 public class App36 { public static void main(String[] args) { System.out.println("update 2023-03-17T14:25:03"); } }
 public class App29 { public static void main(String[] args) { System.out.println("update 2023-03-30T05:37:56"); } }
+public class App71 { public static void main(String[] args) { System.out.println("update 2023-03-30T16:23:30"); } }
